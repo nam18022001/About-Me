02a0679b2040e9d5a971b4e7eab877a2e8ef71eb
nam18022001 <hnvnam.19it3@vku.udn.vn>
2021-03-18 12:08:27 +0700

    About Me but Update

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnDone.setOnClickListener {
+            yourName()
+        }
+    }
+    private fun yourName(){
+        var ourname = editName.text.toString()
+        name.text = "Hello, $ourname"
+        whatname.text = "Hi, my name is $ourname"
     }
 }
